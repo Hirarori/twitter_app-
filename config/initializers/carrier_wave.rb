349eb7b614a41082da374a6eb1de61fe5c889671
Yuta Natori <ny19940618@asagi.waseda.jp>
2019-04-24 02:40:50 +0000

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,17 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-southeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAV4MNDWRRAZKCRMFR'],
-      :aws_secret_access_key => ENV['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']
+      :region                => ENV['S3_REGION'], 
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['yutanatori']
+    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
   end
+
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
 end
